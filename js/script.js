dceffd305992540efbf7d5090bd2108f061c5042
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-02-01 20:30:53 +0100

    lesson 2

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let counter = 0;
+const button = document.querySelector("button");
+const countContainer = document.querySelector(".count");
+
+button.onclick = function (ev) {
+  ev.target.disabled = true;
+  counter = 0;
+
+  const intervalId = setInterval(function () {
+    counter++;
+    countContainer.innerHTML = counter;
+
+    if (counter === 7) {
+      clearInterval(intervalId);
+      button.disabled = false;
+    }
+  }, 1000);
+};
